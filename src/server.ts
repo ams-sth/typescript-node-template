6bb4fb48cc9745c54671e2bd31ab0ed0689009dd
Amsh Shrestha <sthaamsh37@gmail.com>
2025-01-15 09:12:26 +0545

    structure modified for scalability

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
 import app from "./app";
 
-const PORT = 3000;
+dotenv.config();
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
 });
