ccae27c7273c8444ed36bd0682adef2a587a034b
Amsh Shrestha <sthaamsh37@gmail.com>
2025-01-15 10:48:01 +0545

    Moved dotenv to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req:Request, res:Response)=>{
-    res.send("Hello, Typescript with Express!");
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, Typescript with Express!");
+});
 
 export default app;
