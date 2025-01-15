99c38342c57ba1d492bc0e5f00d73a4887616c02
Amsh Shrestha <sthaamsh37@gmail.com>
2025-01-15 08:52:08 +0545

    Made project more scalable

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,4 @@ app.get("/", (req:Request, res:Response)=>{
     res.send("Hello, Typescript with Express!");
 })
 
-const PORT = 3000;
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
